0e25893b3f09fcc14a7b7bf09b25a6f67a0072b2
Sonia Singh <89619373+sonia0409@users.noreply.github.com>
2021-12-01 19:14:58 -0500

    Add files via upload

--- wordsearch.js
+++ wordsearch.js
@@ -2,7 +2,8 @@ const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    }
+    } 
+    return false
 }
 
 module.exports = wordSearch
